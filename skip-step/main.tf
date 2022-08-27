bfb4c758714ddbf55dbe21e86c5a1b14460bf398
Jimmy Lam <lamdaiphong.nz@hotmail.com>
2022-08-27 23:54:26 +1200

    update feature

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -7,10 +7,12 @@ terraform {
       source = "hashicorp/azurerm"
       version = "3.20.0"
     }
-    features {}
   }
 }
 
+provider "azurerm" {
+  features {}
+}
 
 
 resource "azurerm_resource_group" "rg" {
