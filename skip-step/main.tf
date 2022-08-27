f4ec76604d633cc96313db56d0d8759d4a127770
Jimmy Lam <lamdaiphong.nz@hotmail.com>
2022-08-27 23:57:45 +1200

    skipped register

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -2,16 +2,17 @@
 terraform {
     backend "azurerm" {
     }
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.20.0"
+    required_providers {
+        azurerm = {
+        source = "hashicorp/azurerm"
+        version = "3.20.0"
+        }
     }
-  }
 }
 
 provider "azurerm" {
-  features {}
+    skip_provider_registration = "true"
+    features {}
 }
 
 
